87cfdb855d120081960fa16bf93d2b0b9997f252
Trausti Kristjansson <trausti.kristjansson@gmail.com>
2011-09-03 07:24:01 +0000

    Updating MFCC unittest

--- mfcc.js
+++ mfcc.js
@@ -1,40 +1,91 @@
-// MFCC computation.
-//
-// Quick hack based on Matlab code by Dan Ellis.
+/**
+ * Mel frequency cepstrum coefficient (MFCC) computation.
+ * 
+ * Based on Matlab code by Dan Ellis
+ * 
+ * Authors: Trausti Kristjansson, Matthew Lloyd.
+ */
 
-function log10(x) {
+/**
+ * Class for computing MFCC given the power spectrum.
+ */
+function MFCC() {
+  this.sr = 8000;  // Sample rate.
+  this.nfilts = 40;  // Number of Mel filterbanks. Why not 23?
+  this.width = 1.0;  //
+  this.minfrq = 0;
+  this.maxfrq = sr / 2;
+  this.nfft = 256;
+  this.ncep = 13;  // Number of cepstral coefficients.
+  
+  // Center freqs of each FFT bin
+  this.fftfrqs = new Array();
+  for (i = 0; i < this.nfft; i++) {
+    this.fftfrqs[i] = i / this.nfft * this.sr;
+  }
+  
+  // 'Center freqs' of Mel bands - uniformly spaced between limits.
+  var minmel = hz2mel(minfrq);
+  var maxmel = hz2mel(maxfrq);
+  this.binfrqs = new Array();
+  for (i = 0; i <= this.nfilts + 1; i++) {
+    this.binfrqs[i] = mel2hz(minmel + i / (this.nfilts + 1) * (maxmel - minmel));
+  }
+}
+
+
+
+/**
+ * Log10.
+ */
+MFCC.prototype.log10 = function(x) {
   return Math.log(x) / Math.LN10;
 }
 
-function hz2mel(f) {
+function TEST_log10() {
+  TEST_TITLE();
+  mfcc = new MFCC();
+  EXPECT_NEAR(0.0, mfcc.log10(1.0), "Expected log10(1) to be 0.");
+  EXPECT_NEAR(1.0, mfcc.log10(10.0), "Expected log10(10) to be 1.");
+  EXPECT_NEAR(2.0, mfcc.log10(100.0), "Expected log10(100) to be 2.");
+  TEST_SUMMARY();
+}
+
+/**
+ * Hertz to Mel frequency function.
+ */
+MFCC.prototype.hz2mel = function(f) {
   // HTK version
   return 2595.0 * log10(1 + f / 700.0);
 }
 
-function mel2hz(m) {
-  return 700 * (Math.pow(10, m / 2595) - 1);
-}
-var sr = 8000;
-var nfilts = 40; // 23?
-var width = 1.0;
-var minfrq = 0;
-var maxfrq = sr / 2;
-var nfft = 256;
-var ncep = 13;
-// Center freqs of each FFT bin
-fftfrqs = new Array();
-for (i = 0; i < nfft; i++) {
-  fftfrqs[i] = i / nfft * sr;
-}
-// 'Center freqs' of mel bands - uniformly spaced between limits
-minmel = hz2mel(minfrq);
-maxmel = hz2mel(maxfrq);
-binfrqs = new Array();
-for (i = 0; i <= nfilts + 1; i++) {
-  binfrqs[i] = mel2hz(minmel + i / (nfilts + 1) * (maxmel - minmel));
+function TEST_hz2mel() {
+  TEST_TITLE();
+  mfcc = new MFCC();
+  EXPECT_NEAR(3000, hz2mel(3000), "Exepcted the ?? hz to map to ?? on Mel scale");
+  TEST_SUMMARY();
 }
 
-function fft2mel(power_spectrum) {
+
+/**
+ * Mel to Hertz function.
+ */
+MFCC.prototype.mel2hz = function(m) {
+  return 700 * (Math.pow(10, m / 2595) - 1);
+}
+
+function TEST_mel2hz() {
+  TEST_TITLE();
+  mfcc = new MFCC();
+  for (f = 100; f < 8000; f += 1000)
+  EXPECT_NEAR(f, mfcc.mel2hz(mffc.hz2mel(f)), "Expected f = mel2hz(hz2mel(f))");
+  TEST_SUMMARY();
+}
+
+/**
+ * Convert fft to Mel.
+ */ 
+MFCC.prototype.fft2mel = function(power_spectrum) {
   var filterbank = new Array();
   for (i = 0; i < nfilts; i++) {
     var total = 0;
@@ -49,14 +100,34 @@ function fft2mel(power_spectrum) {
     }
     filterbank[i] = log10(total);
   }
+}
+
+function TEST_fft2mel() {
+  TEST_TITLE();
+  
+  
+  TEST_SUMMARY();
+}
+
+/**
+ * Take Discrete Cosine transform of Mel filter values.
+ */
+MFCC.prototype.mel2dct = function(filterbank) {
   var dct = new Array();
-  for (i = 0; i < ncep; i++) {
+  for (i = 0; i < this.ncep; i++) {
     var total = 0;
-    for (j = 0; j < nfilts; j++) {
-      // cos((i-1)*    [1:2:(2*nrow-1)]/(2*nrow)      *pi) * sqrt(2/nrow);
-      total += filterbank[j] * Math.cos(i * (1 + 2 * j) / (2 * nfilts) * Math.PI) * Math.sqrt(2.0 / nfilts) / Math.sqrt(2.0);
+    for (j = 0; j < this.nfilts; j++) {
+      total += filterbank[j] * Math.cos(i * (1 + 2 * j) 
+          / (2 * this.nfilts) * Math.PI) * Math.sqrt(2.0 / this.nfilts) / Math.sqrt(2.0);
     }
     dct[i] = total;
   }
   return dct;
+}
+
+function TEST_mel2dct() {
+  TEST_TITLE();
+  
+  
+  TEST_SUMMARY();
 }
